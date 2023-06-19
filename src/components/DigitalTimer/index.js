d936d081cd07bad91304306d1245a1c098872516
Winstone <winstonwork57@gmail.com>
2023-06-19 06:23:27 +0000

    digital timer

--- src/components/DigitalTimer/index.js
+++ src/components/DigitalTimer/index.js
@@ -4,14 +4,30 @@ import {Component} from 'react'
 import './index.css'
 
 class DigitalTimer extends Component {
-  state = {timerStart: false, limit: 25, timer: '25:00'}
+  state = {
+    timerStart: false,
+    limit: 25,
+    minutes: 25,
+    seconds: 0,
+  }
 
   onToggleStart = () => {
+    const {timerStart} = this.state
+    if (timerStart === true) {
+      this.timerId = setInterval(this.tick(), 1000)
+      console.log('timer started')
+    } else {
+      clearInterval(this.timerId)
+      console.log('timer pause')
+    }
+
     this.setState(prevState => ({timerStart: !prevState.timerStart}))
   }
 
   onReset = () => {
-    this.setState({limit: 25, timer: '25:00'})
+    clearInterval(this.timerId)
+    console.log('timer reset')
+    this.setState({limit: 25, minutes: 25, seconds: 0})
   }
 
   onDecrease = () => {
@@ -28,21 +44,42 @@ class DigitalTimer extends Component {
     }
   }
 
+  tick = () => {
+    const {seconds, minutes} = this.state
+    if (seconds === 0) {
+      this.setState(prevState => ({
+        seconds: 59,
+        minutes: prevState.minutes - 1,
+      }))
+    } else if ((minutes === 0, seconds === 0)) {
+      clearInterval(this.timerId)
+      this.setState({seconds: 0, minutes: 0})
+    } else {
+      this.setState(prevState => ({
+        seconds: prevState.seconds - 1,
+      }))
+    }
+  }
+
   render() {
-    const {timerStart, limit, timer} = this.state
+    const {timerStart, limit, minutes, seconds} = this.state
     const startPauseImg = timerStart
       ? 'https://assets.ccbp.in/frontend/react-js/pause-icon-img.png'
       : 'https://assets.ccbp.in/frontend/react-js/play-icon-img.png'
     const startPauseAlt = timerStart ? 'pause icon' : 'play icon'
     const startPauseText = timerStart ? 'Pause' : 'Start'
     const timerStatusText = timerStart ? 'Running' : 'Paused'
+    const stringifiedMinutes = minutes > 9 ? minutes : `0${minutes}`
+    const stringifiedSeconds = seconds > 9 ? seconds : `0${seconds}`
     return (
       <div className="app-container">
         <h1 className="app-title">Digital Timer</h1>
         <div className="app-body">
           <div className="app-timer">
             <div className="timer">
-              <h1 className="timer-display">{timer}</h1>
+              <h1 className="timer-display">
+                {stringifiedMinutes}:{stringifiedSeconds}
+              </h1>
               <p className="timer-status">{timerStatusText}</p>
             </div>
           </div>
